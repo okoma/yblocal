5d0bf11938a74889a289a80b85fbce6bb6cd4fe8
Okoma <okoma078@gmail.com>
2026-01-28 03:20:23 +0100

    Update QuoteResponseResource navigation sort order to 0

--- app/Filament/Business/Resources/QuoteResponseResource.php
+++ app/Filament/Business/Resources/QuoteResponseResource.php
@@ -25,7 +25,7 @@ class QuoteResponseResource extends Resource
     
     protected static ?string $navigationGroup = 'Quotes';
     
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 0;
 
     public static function getEloquentQuery(): Builder
     {
