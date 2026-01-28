ed0810541476026886c46a4fd20118e052e436e6
Okoma <okoma078@gmail.com>
2026-01-28 02:20:17 +0100

    Add create button to quote requests list page header
    - Added getHeaderActions() to ListQuoteRequests page
    - Customers can now create multiple quote requests
    - 'New Quote Request' button always visible in header

--- app/Filament/Customer/Resources/QuoteRequestResource/Pages/ListQuoteRequests.php
+++ app/Filament/Customer/Resources/QuoteRequestResource/Pages/ListQuoteRequests.php
@@ -4,8 +4,18 @@ namespace App\Filament\Customer\Resources\QuoteRequestResource\Pages;
 
 use App\Filament\Customer\Resources\QuoteRequestResource;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions;
 
 class ListQuoteRequests extends ListRecords
 {
     protected static string $resource = QuoteRequestResource::class;
+    
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('New Quote Request')
+                ->icon('heroicon-o-plus-circle'),
+        ];
+    }
 }
