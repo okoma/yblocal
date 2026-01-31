80e25f661a26179effff983f37af62b72150b7b8
Okoma <okoma078@gmail.com>
2026-01-31 19:24:29 +0100

    layout: update business layout

--- resources/views/layouts/business.blade.php
+++ resources/views/layouts/business.blade.php
@@ -73,7 +73,7 @@
     </header>
 
     <!-- Main Content -->
-    <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+    <main class="">
         @yield('content')
         {{ $slot ?? '' }}
     </main>
